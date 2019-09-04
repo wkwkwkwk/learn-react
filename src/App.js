5d9e6f6bd05e9865b445066f55b4c0d87a988bb0
wkwkwkwk <aula.mh46@gmail.com>
2019-09-04 14:08:02 +0700

    Adding color slider

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Palette {...seedColors[4]} />
+        <Palette palette={generatePalette(seedColors[4])} />
       </div>
     );
   }
