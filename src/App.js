8a75e8e6d6e8c16b15ce5ff74dbc67c6733e3cd3
wkwkwkwk <aula.mh46@gmail.com>
2019-09-05 08:47:31 +0700

    Add home page and links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
+import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
@@ -17,7 +18,7 @@ class App extends Component {
         <Route 
           exact 
           path="/" 
-          render={() => <h1>Home</h1>}
+          render={() => <PaletteList palettes={seedColors} />}
         />
         <Route 
           exact 
