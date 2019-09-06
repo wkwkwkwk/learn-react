75adff52960f6026812024a01b69abe000ced453
wkwkwkwk <aula.mh46@gmail.com>
2019-09-06 10:42:23 +0700

    Styling SingleColorPalette with JSS

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
-import './Palette.css';
 import PaletteFooter from './PaletteFooter';
+import { withStyles } from '@material-ui/styles';
+import './Palette.css';
+
+const styles ={
+    Palette: {
+        height: "100vh", 
+        display: "flex", 
+        flexDirection: "column"
+    },
+    colors: {
+        height: "90%"
+    }
+}
 
 class Palette extends Component {
     constructor(props) {
@@ -25,6 +37,7 @@ class Palette extends Component {
 
     render() {
         const { colors, paletteName, emoji, id } = this.props.palette;
+        const { classes } = this.props;
         const { level, format } = this.state;
         const colorBoxes = colors[level].map(color => (
             <ColorBox 
@@ -37,14 +50,14 @@ class Palette extends Component {
             />
         ));
         return(
-            <div className="Palette">
+            <div className={classes.Palette}>
                 <Navbar 
                     level={level} 
                     changeLevel={this.changeLevel} 
                     handleChange={this.changeFormat} 
                     showingAllColors={true} 
                 />
-                <div className="Palette-colors">
+                <div className={classes.colors}>
                     {colorBoxes}
                 </div>
                 <PaletteFooter paletteName={paletteName} emoji={emoji} />
@@ -53,4 +66,4 @@ class Palette extends Component {
     }
 }
 
-export default Palette;
+export default withStyles(styles)(Palette);
