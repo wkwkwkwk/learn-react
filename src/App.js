83cab5248d739deeb2c7423dd9f780a320e20be3
wkwkwkwk <aula.mh46@gmail.com>
2019-09-06 16:39:07 +0700

    Linked to NewPaletteForm

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import PaletteList from './PaletteList';
-import seedColors from './seedColors';
-import { generatePalette } from './colorHelpers';
 import SingleColorPalette from './SingleColorPalette';
+import seedColors from './seedColors';
+import NewPaletteForm from './NewPaletteForm';
+import { generatePalette } from './colorHelpers';
 
 class App extends Component {
   findPalette(id) {
@@ -16,6 +17,11 @@ class App extends Component {
   render() {
     return (
       <Switch>
+        <Route 
+          exact 
+          path="/palette/new" 
+          render={() => <NewPaletteForm />}
+        />
         <Route 
           exact 
           path="/" 
