09c7a5621f32aa72a1f1e5afc943c27ee00fd36f
wkwkwkwk <aula.mh46@gmail.com>
2019-09-12 10:49:00 +0700

    Save to local storage

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import { generatePalette } from './colorHelpers';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { palettes: seedColors };
+    const savePalettes = JSON.parse(window.localStorage.getItem("palettes"));
+    this.state = { palettes: savePalettes || seedColors };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
   }
@@ -22,7 +23,11 @@ class App extends Component {
   }
 
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] });
+    this.setState({ palettes: [...this.state.palettes, newPalette] }, this.syncLocalStorage);
+  }
+
+  syncLocalStorage() {
+    window.localStorage.setItem("palettes", JSON.stringify(this.state.palettes));
   }
 
   render() {
