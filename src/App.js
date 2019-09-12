8abd7f1e197a68db7da6362331eb8a3060715051
wkwkwkwk <aula.mh46@gmail.com>
2019-09-12 11:22:51 +0700

    Add delete MiniPalette function

--- src/App.js
+++ src/App.js
@@ -14,12 +14,20 @@ class App extends Component {
     this.state = { palettes: savePalettes || seedColors };
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
+    this.deletePalette = this.deletePalette.bind(this);
   }
 
   findPalette(id) {
     return this.state.palettes.find(function(palette) {
       return palette.id === id;
     });
+  }
+
+  deletePalette(id) {
+    this.setState(
+      st => ({ palettes: st.palettes.filter(palette => palette.id !== id) }), 
+      this.syncLocalStorage
+    );
   }
 
   savePalette(newPalette) {
@@ -41,7 +49,7 @@ class App extends Component {
         <Route 
           exact 
           path="/" 
-          render={(routeProps) => <PaletteList palettes={this.state.palettes} {...routeProps} />}
+          render={(routeProps) => <PaletteList palettes={this.state.palettes} deletePalette={this.deletePalette} {...routeProps} />}
         />
         <Route 
           exact 
