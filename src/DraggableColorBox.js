16772d2b49ae14b07aa9895c23f22ca9d26b7699
wkwkwkwk <aula.mh46@gmail.com>
2019-09-12 09:59:50 +0700

    Create move JSS into sparate files

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -2,40 +2,8 @@ import React from 'react';
 import { SortableElement } from 'react-sortable-hoc';
 import { withStyles } from '@material-ui/styles';
 import DeleteIcon from '@material-ui/icons/Delete';
+import styles from './styles/DraggableColorBoxStyles';
 // import { classes } from 'istanbul-lib-coverage';
-
-const styles = {
-    root: {
-        height: "20%", 
-        width: "25%", 
-        margin: "0 auto", 
-        display: "inline-block", 
-        position: "relative", 
-        cursor: "pointer", 
-        marginBottom: "-3.5px",
-        "&:hover svg": {
-            color: "white", 
-            transform: "scale(1.5)"
-        }
-    },
-    boxContent: {
-        position: "absolute", 
-        width: "100%", 
-        left: "0px", 
-        bottom: "0px", 
-        padding: "10px", 
-        color: "black", 
-        letterSpacing: "1px", 
-        textTransform: "uppercase", 
-        fontSize: "12px", 
-        display: "flex", 
-        justifyContent: "space-between"
-    }, 
-    deleteIcon: {
-        color: "rgba(0, 0, 0, 0.5)", 
-        transotion: "all 0.3s ease-in-out"
-    }
-};
 
 const DraggableColorBox = SortableElement(props => {
     const { classes, handleClick, name, color } = props;
